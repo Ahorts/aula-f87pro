9f2d7a6c90e872ad69f5a3ab738f27e61a3ca626
Ahorts <godswayahorts@gmail.com>
2025-05-27 19:33:44 +0000

    chore: add setup.py for Aula F87 Pro CLI tool with package configuration

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aula-f87pro-cli',
+    version='0.1.0',
+    author='Ahorts',
+    author_email='godswayahorts@gmail.com',
+    description='CLI tool for controlling RGB of Aula F87 Pro keyboard',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'hidapi', 
+    ],
+    entry_points={
+        'console_scripts': [
+            'aula-f87pro-cli=aula_f87pro.cli:main',  # Fixed import path
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9', 
+)
